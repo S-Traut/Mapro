9036b79bcf5813d3400bb7de8857b389e490920d
Stéphane Traut <stephane.traut@gmail.com>
2020-12-23 13:58:51 +0100

    redirection si succès location vers /home

--- public/js/localisation.js
+++ public/js/localisation.js
@@ -4,6 +4,7 @@ locateButton.onclick = onLocate;
 function onLocate() {
     if ("geolocation" in navigator) {
         console.log(navigator.geolocation.getCurrentPosition(success, error, options));
+
     }
 }
 
@@ -14,6 +15,8 @@ function success(pos) {
     console.log(`Latitude : ${crd.latitude}`);
     console.log(`Longitude : ${crd.longitude}`);
     console.log(`La précision est de ${crd.accuracy} mètres.`);
+    
+    location.href = "/home";
 }
 
 function error(err) {
