050d3a04e83d069855afbca588e5771875cc147b
Stéphane Traut <stephane.traut@gmail.com>
2021-01-05 16:21:37 +0100

    Creation form

--- src/Controller/MagasinController.php
+++ src/Controller/MagasinController.php
@@ -2,11 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\Magasin;
+use App\Form\CreationMagasinType;
 use App\Repository\MagasinRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
 
 class MagasinController extends AbstractController
 {
@@ -19,8 +22,33 @@ class MagasinController extends AbstractController
         {
             throw $this->createNotFoundException('Magasin Inexistant !');
         }
-        return $this->render('pages/magasin.html.twig', [
+        return $this->render('magasin/magasin.html.twig', [
             'Magasin' => $magasinRepository->find($id)
+        ]);
+    }
+
+    /**
+     * @Route("/magasin/new")
+     * 
+     * @return void
+     */
+    public function new(Request $request, EntityManagerInterface $em)
+    {
+        $magasin = new Magasin();
+
+        $form = $this->createForm(CreationMagasinType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($magasin);
+            $em->flush();
+            return new Response('OK');
+        }
+
+
+        return $this->render('magasin/new.html.twig' , [
+            'magasin' => $magasin,
+            'form' => $form->createView()
         ]);
     }
 }
---
+++ src/Form/CreationMagasinType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Magasin;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CreationMagasinType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nom')
+            ->add('description')
+            ->add('tel')
+            ->add('email')
+            ->add('siren')
+            ->add('etat')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Magasin::class,
+        ]);
+    }
+}
