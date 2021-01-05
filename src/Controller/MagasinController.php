998127f492978cb06c94d5f1e63ccdc99639eac9
Maxime <maxime.vilars@gmail.com>
2021-01-05 13:56:29 +0100

    Création du controller pour les magasins

---
+++ src/Controller/MagasinController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\MagasinRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MagasinController extends AbstractController
+{
+    /**
+     * @Route("/magasin/{id<\d+>}")
+     */
+    public function show(MagasinRepository $magasinRepository, $id)
+    {   
+        if(!$magasinRepository->find($id))
+        {
+            throw $this->createNotFoundException('Magasin Inexistant !');
+        }
+        return $this->render('pages/magasin.html.twig', [
+            'Magasin' => $magasinRepository->find($id)
+        ]);
+    }
+}
