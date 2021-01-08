3abb09a32775534376e6f13c9ae78ec05cab8d57
Stéphane Traut <stephane.traut@gmail.com>
2021-01-08 20:47:29 +0100

    Fixture Article

---
+++ src/DataFixtures/TypeArticleFixtures.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\TypeArticle;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Bundle\FixturesBundle\FixtureGroupInterface;
+use Doctrine\Persistence\ObjectManager;
+
+class TypeArticleFixtures extends Fixture implements FixtureGroupInterface
+{
+    public function load(ObjectManager $manager)
+    {
+        $typeArticles = ["Nourriture", "Vêtement", "Jouets"];
+        foreach($typeArticles as $typeArticle)
+        {
+            $typeArticleFixture = new TypeArticle();
+            $typeArticleFixture->setType($typeArticle);
+            $manager->persist($typeArticleFixture);
+        }
+        $manager->flush();
+    }
+
+    public static function getGroups(): array
+    {
+        return ['dev'];
+    }
+}
