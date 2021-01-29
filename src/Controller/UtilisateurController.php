14d426cbc96370f193cf6d519725b2e8bdfa036c
TRAUT Stéphane <stephane.traut@gmail.com>
2021-01-29 11:28:11 +0100

    Modify password

--- src/Controller/UtilisateurController.php
+++ src/Controller/UtilisateurController.php
@@ -2,39 +2,57 @@
 
 namespace App\Controller;
 
+use App\Form\ChangePasswordType;
 use Symfony\Component\HttpFoundation\Request;
 use App\Form\UserType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\CurrencyType;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\PasswordEncoderInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class UtilisateurController extends AbstractController
 {
     /**
      * @Route("/me", name="menu")
      */
-    public function index(Request $request, EntityManagerInterface $em): Response
+    public function index(Request $request, EntityManagerInterface $em, UserPasswordEncoderInterface $passwordEncoder): Response
     {
         if($this->isGranted('ROLE_USER') == false)
             return $this->redirectToRoute("landing");
         
         $utilisateur = $this->getUser();
         $form = $this->createForm(UserType::class, $utilisateur);
+        $form_password = $this->createForm(ChangePasswordType::class);
         $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
+        if ($form->isSubmitted() && $form->isValid()) 
+        {
             $em->persist($utilisateur);
             $em->flush();
             return $this->redirectToRoute("menu");
         }
 
-        dump($utilisateur->getLocalisation()[0]);
+        $form_password->handleRequest($request);
+        if($form_password->isSubmitted() && $form_password->isValid())
+        {
+            $requestPassword = $request->get('change_password');
+            $currentPassword = $passwordEncoder->isPasswordValid($utilisateur, $requestPassword['curPassword']);
+            if($currentPassword && $requestPassword['newPassword'] == $requestPassword['newConfirm'])
+            {
+                $encodedPassword = $passwordEncoder->encodePassword($utilisateur, $requestPassword['newConfirm']);
+                $utilisateur->setPassword($encodedPassword);
+                $em->flush();
+            }
+        }
 
         return $this->render('utilisateur/index.html.twig', [
             'annonce' => $utilisateur,
-            'form' => $form->createView(), // la méthode createView est très important ! Si tu l'oublie, Twig ne pourra pas interpréter le formulaire
-            'adresses' => $utilisateur->getLocalisation()
+            'form' => $form->createView(),
+            'adresses' => $utilisateur->getLocalisation(),
+            'password_form' => $form_password->createView()
         ]);
     }
 
