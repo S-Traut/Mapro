de74e2a7476386774ad031dc8887f67c64b3346d
lionel harry <lharry1@hotmail.fr>
2021-01-29 10:32:50 +0100

    séparation client vendeur lors de l'inscription

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -59,7 +59,7 @@ class RegistrationController extends AbstractController
                         $form->get('plainPassword')->getData()
                     )
                 );
-                $user->setRoles(['ROLE_VENDEUR']);
+                $user->setRoles(['ROLE_CLIENT']);
 
                 $entityManager = $this->getDoctrine()->getManager();
                 $entityManager->persist($user);
