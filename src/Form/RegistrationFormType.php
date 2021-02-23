15d7e4e1e34dc14e6472829b0809a7a9fb71138e
TRAUT Stéphane <stephane.traut@gmail.com>
2021-02-23 17:57:38 +0100

    Pushing changes

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -22,8 +22,6 @@ class RegistrationFormType extends AbstractType
             ->add('email')
             ->add('vendeur', CheckboxType::class, ['mapped' => false, 'required' => false,])
             ->add('plainPassword', PasswordType::class, [
-                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
                 'mapped' => false,
                 'constraints' => [
                     new NotBlank([
@@ -32,7 +30,6 @@ class RegistrationFormType extends AbstractType
                     new Length([
                         'min' => 6,
                         'minMessage' => 'Votre mot de passe devrait contenir au moins {{ limit }} caractères',
-                        // max length allowed by Symfony for security reasons
                         'max' => 4096,
                     ]),
                 ],
