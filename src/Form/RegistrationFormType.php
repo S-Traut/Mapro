3162e0c843b43b58e4ec31d5ffaaf8a01b0267f7
s-traut <stephane.traut@gmail.com>
2021-02-26 11:50:08 +0100

    Modification des formulaires d'inscription

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -6,6 +6,8 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
@@ -17,19 +19,47 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('prenom')
-            ->add('nom')
-            ->add('email')
+            ->add('prenom', TextType::class, [
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez indiquer un prénom valide.'
+                    ]),
+                    new Length([
+                        'min' => 2,
+                        'minMessage' => 'Votre prénom doit contenir au moins {{ limit }} caractères.',
+                        'max' => 20,
+                    ])
+                ],
+            ])
+            ->add('nom', TextType::class, [
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez indiquer un nom valide.'
+                    ]),
+                    new Length([
+                        'min' => 2,
+                        'minMessage' => 'Votre nom doit contenir au moins {{ limit }} caractères.',
+                        'max' => 20,
+                    ])
+                ],
+            ])
+            ->add('email', EmailType::class, [
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez ne pas laisser le champ vide.'
+                    ])
+                ],
+            ])
             ->add('vendeur', CheckboxType::class, ['mapped' => false, 'required' => false,])
             ->add('plainPassword', PasswordType::class, [
                 'mapped' => false,
                 'constraints' => [
                     new NotBlank([
-                        'message' => 'Saisissez un mot de passe',
+                        'message' => 'Veuillez ne pas laisser le champ vide.',
                     ]),
                     new Length([
                         'min' => 6,
-                        'minMessage' => 'Votre mot de passe devrait contenir au moins {{ limit }} caractères',
+                        'minMessage' => 'Votre mot de passe doit contenir au moins {{ limit }} caractères.',
                         'max' => 4096,
                     ]),
                 ],
