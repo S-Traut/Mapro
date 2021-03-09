db50952bd361b7b228c18cb4df3b7f22600f56fb
MaximeVLR <maxime.vilars@gmail.com>
2021-03-09 13:55:09 +0100

    Modification fixture Client

--- src/DataFixtures/UtilisateurFixtures.php
+++ src/DataFixtures/UtilisateurFixtures.php
@@ -18,15 +18,54 @@ class UtilisateurFixtures extends Fixture
 
     public function load(ObjectManager $manager)
     {
+        //User Admin        
+        $utilisateurTest = new User();
+        $utilisateurTest->setRoles(['ROLE_ADMIN']);
+        $utilisateurTest->setNom("UtilisateurAdminNom");
+        $utilisateurTest->setPrenom("UtilisateurAdminPrénom");
+        $password = $this->encoder->encodePassword($utilisateurTest, 'admin');
+        $utilisateurTest->setPassword($password);
+        $utilisateurTest->setEmail("admin@admin.com");
+        $manager->persist($utilisateurTest);
+
+        //User Vendeur
         $utilisateurTest = new User();
         $utilisateurTest->setRoles(['ROLE_VENDEUR']);
-        $utilisateurTest->setNom("UtilisateurTest");
-        $utilisateurTest->setPrenom("UtilisateurTestPrénom");
-        $password = $this->encoder->encodePassword($utilisateurTest, 'test');
+        $utilisateurTest->setNom("TAUFF");
+        $utilisateurTest->setPrenom("Kriss");
+        $password = $this->encoder->encodePassword($utilisateurTest, 'Kriss');
         $utilisateurTest->setPassword($password);
-        $utilisateurTest->setEmail("test@test.com");
-        
+        $utilisateurTest->setEmail("kriss@gmail.com");
         $manager->persist($utilisateurTest);
+
+        $utilisateurTest = new User();
+        $utilisateurTest->setRoles(['ROLE_VENDEUR']);
+        $utilisateurTest->setNom("VILAR");
+        $utilisateurTest->setPrenom("Maxime");
+        $password = $this->encoder->encodePassword($utilisateurTest, 'Maxime');
+        $utilisateurTest->setPassword($password);
+        $utilisateurTest->setEmail("maxime.vilars@gmail.com");
+        $manager->persist($utilisateurTest);
+
+        // User Client
+        $utilisateurTest = new User();
+        $utilisateurTest->setRoles(['ROLE_CLIENT']);
+        $utilisateurTest->setNom("KEVIN");
+        $utilisateurTest->setPrenom("Jean");
+        $password = $this->encoder->encodePassword($utilisateurTest, 'Jean');
+        $utilisateurTest->setPassword($password);
+        $utilisateurTest->setEmail("jean@gmail.com");
+        $manager->persist($utilisateurTest);
+
+        $utilisateurTest = new User();
+        $utilisateurTest->setRoles(['ROLE_CLIENT']);
+        $utilisateurTest->setNom("POTTER");
+        $utilisateurTest->setPrenom("Harry");
+        $password = $this->encoder->encodePassword($utilisateurTest, 'Harry');
+        $utilisateurTest->setPassword($password);
+        $utilisateurTest->setEmail("harry@gmail.com");
+        $manager->persist($utilisateurTest);
+
         $manager->flush();
     }
 }
